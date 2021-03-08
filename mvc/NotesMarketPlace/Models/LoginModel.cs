c6b0ae76cd182cf870d384d5e9d3f6b7a2858586
Darshi2599 <dsanghvi2599@gmail.com>
2021-03-08 09:56:48 +0530

    First MVC Submission

---
+++ mvc/NotesMarketPlace/Models/LoginModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NotesMarketPlace.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string EmailID { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        /*[RegularExpression(@"^((?=.*[0-9])(?=.*[a-z])(?=.*[@#$%^&-+=()])(?=\\S+$).{6, 24}$")]*/
+        public string Password { get; set; }
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+
+    }
+}
---
+++ mvc/NotesMarketPlace/Models/SignUpModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace NotesMarketPlace.Models
+{
+    public class SignUpModel
+    {
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use letters only please")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string EmailID { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [RegularExpression(@"^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*\d)(?=.*[@$!%*#?&])[a-zA-Z\d@$!%*#?&]{6,24}$", ErrorMessage = "It must be between 6 and 24 characters long & it must have at least 1 lowercase character & it must have at least 1 special character & it must have at least 1-digit character & It must not contain whitespaces")]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
